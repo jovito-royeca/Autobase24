7353147a3c97d564cfccc471408297c07a5d463f
Jovito Royeca <jovit.royeca@gmail.com>
2017-05-16 21:46:02 +0800

    feat: Added view controllers

---
+++ Autobase24/FavoritesViewController.swift
@@ -0,0 +1,48 @@
+//
+//  FavoritesViewController.swift
+//  Autobase24
+//
+//  Created by Jovito Royeca on 16/05/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: UITableViewDataSource
+extension FavoritesViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+extension FavoritesViewController : UITableViewDelegate {
+    
+}
+
