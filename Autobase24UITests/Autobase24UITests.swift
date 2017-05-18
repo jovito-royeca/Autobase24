b4b7f4f45ac659d6bbb7c7885d302e9028a652d1
Jovito Royeca <jovit.royeca@gmail.com>
2017-05-18 21:45:14 +0800

    feat: Added UI Tests

--- Autobase24UITests/Autobase24UITests.swift
+++ Autobase24UITests/Autobase24UITests.swift
@@ -31,6 +31,77 @@ class Autobase24UITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIApplication().tabBars.buttons["Favorites"].tap()
+        
+        
+        
+        
     }
     
+    func testSorting() {
+        let app = XCUIApplication()
+        var sortButton = app.navigationBars["Autobase24.CarsView"].buttons.element(boundBy: 0)
+        sortButton.tap()
+        sortButton.tap()
+        
+        // go to Favorites tab
+        app.tabBars.buttons["Favorites"].tap()
+        sortButton = app.navigationBars["Autobase24.FavoritesView"].buttons.element(boundBy: 0)
+        sortButton.tap()
+        sortButton.tap()
+    }
+
+    func testFiltering() {
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        
+        tablesQuery.buttons["bmw"].tap()
+        tablesQuery.buttons["audi"].tap()
+        tablesQuery.buttons["mercedes benz"].tap()
+        tablesQuery.buttons["all cars"].tap()
+    }
+    
+    func testAddFavorites() {
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        
+        // tap on the switches
+        for i in 0..<tablesQuery.cells.count {
+            let cell = tablesQuery.cells.element(boundBy: i)
+            for j in 0..<cell.switches.count {
+                let fSwitch = cell.switches.element(boundBy: j)
+                if let on = (fSwitch.value! as AnyObject).boolValue {
+                    if !on {
+                        fSwitch.tap()
+                    }
+                }
+            }
+        }
+    }
+    
+    func testRemoveFavorites() {
+        let app = XCUIApplication()
+        
+        app.tabBars.buttons["Favorites"].tap()
+        
+        let tablesQuery = app.tables
+        
+        for i in 0..<tablesQuery.cells.count {
+            let cell = tablesQuery.cells.element(boundBy: i)
+            cell.swipeLeft()
+            cell.buttons["Delete"].tap()
+        }
+    }
+    
+    func testCalculation() {
+        let app = XCUIApplication()
+        
+        // go to Favorites tab and do calculation
+        app.tabBars.buttons["Favorites"].tap()
+        let textField = app.textFields["amountTextField"]
+        textField.press(forDuration: 1.0)
+        textField.typeText("10000")
+        app.buttons["Calculate"].tap()
+    }
 }
